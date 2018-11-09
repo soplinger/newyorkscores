477d3626aa2f10da2592f385c7858bc8b09635ae
soplinger <so367@pvsd.org>
2018-11-09 11:22:22 -0500

    sdasd

--- src/com/company/Schools.java
+++ src/com/company/Schools.java
@@ -4,16 +4,52 @@ package com.company;
  * Created by so367 on 11/8/18.
  */
 public class Schools {
-    public double math;
-    public double reading;
-    public double writing;
+    public int math;
+    public int reading;
+    public int writing;
+    public int satAverage;
+    public static int satHighest;
+    public static int satSecond;
+    public static int satThird;
+    public static int satTotal;
+    public static int satCount;
+    public static double stateAverage;
+    public static String highestSchool;
+    public static String secondSchool;
+    public static String thirdSchool;
+
+    public String schoolName;
 
 
-    public Schools(double a, double b, double c)
-    {
-        math=a;
-        reading=b;
-        writing=c;
+    public Schools(String n, int a, int b, int c) {
+        math = a;
+        reading = b;
+        writing = c;
+        schoolName = n;
+        satAverage = math + reading + writing;
+        if (satAverage > satHighest){
+            satThird = satSecond;
+            thirdSchool = secondSchool;
+            satSecond = satHighest;
+            secondSchool = highestSchool;
+            satHighest = satAverage;
+            highestSchool = schoolName;
+        }
+        else if (satAverage > satSecond){
+            satThird = satSecond;
+            thirdSchool = secondSchool;
+            satSecond = satAverage;
+            secondSchool = schoolName;
+        }
+        else if (satAverage > satThird){
+            satThird = satAverage;
+            thirdSchool = schoolName;
+        }
+        if (satAverage > 0){
+            satCount++;
+            satTotal += satAverage;
+            stateAverage = satTotal/satCount;
+        }
     }
 
 }
