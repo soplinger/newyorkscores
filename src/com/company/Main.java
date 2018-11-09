8141aa41e99800e96ab3f5b96c1465dde57a3a40
soplinger <so367@pvsd.org>
2018-11-09 11:22:44 -0500

    yes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,55 +3,79 @@ import java.util.*;
 import java.io.*;
 public class Main {
 
-    public static void main(String[] args) throws IOException,InputMismatchException
-    {
-	    Scanner s = new Scanner(new File("scores.txt"));
+    public static void main(String[] args) throws IOException, InputMismatchException {
+        Scanner s = new Scanner(new File("scores.txt"));
         int maxIndx = -1;
         String a[] = new String[100000000];
-        Schools b[]= new Schools[10000000];
-        double math = 0, reading = 0, writing = 0;
+        Schools b[] = new Schools[10000000];
+        int math = 0, reading = 0, writing = 0;
+        String schoolName = "";
 
-        while (s.hasNext())
-        {
+        while (s.hasNext()) {
             maxIndx++;
             a[maxIndx] = s.nextLine();
         }
 
         s.close();
 
-        System.out.println(a[3]);
 
 
-         for (int i=0; i<=maxIndx;i++)
-        {
+        for (int i = 1; i <= maxIndx; i++) {
             Scanner s2 = new Scanner(a[i]);
             s2.useDelimiter("\t");
-            System.out.println(s2.next());
-            System.out.println(s2.next());
 
-            Schools aa = new Scanner()
+            s2.next();
+            schoolName = s2.next();
 
 
-            //for (int j = 0; j < 18; j++)
-           // {
-           //     if(s2.hasNext())
-          //          s2.next();
-          //  }
+            for (int j = 0; j < 16; j++) {
+                if (s2.hasNext())
+                    s2.next();
+            }
 
-          //  if(s2.hasNext())
-          //      math = s2.nextDouble();
-         //   if(s2.hasNext())
-            //    reading = s2.nextDouble();
-         //   if(s2.hasNext())
-         //       writing = s2.nextDouble();
-//
-     //       b[i]= new Schools(math,reading,writing);
+
+            String math2 = "0", reading2 = "0", writing2 = "0";
+
+            if (s2.hasNext()) {
+                math2 = s2.next();
+                if (math2.equals("")){
+                    math = 0;
+                }
+                else{
+                math = Integer.valueOf(math2);}
+
+            }
+            if (s2.hasNext()) {
+                reading2 = s2.next();
+                if (reading2.equals("")){
+                    reading = 0;
+                }
+                else{
+                    reading = Integer.valueOf(reading2);}
+            }
+            if (s2.hasNext()) {
+                writing2 = s2.next();
+
+
+                if (writing2.equals("")){
+                    writing = 0;
+                }
+                else{
+                    writing = Integer.valueOf(writing2);}
+            }
+            b[i] = new Schools(schoolName, math, reading, writing);
+
+
         }
-
-        for(int i=0; i<=maxIndx;i++)
-        {
-           System.out.println(b[i].math);
+        System.out.println("School SAT Average");
+        for (int k = 1; k < maxIndx; k++) {
+            System.out.println(b[k].schoolName + "\t" + b[k].satAverage);
         }
-
+        System.out.println("");
+        System.out.println("Highest SAT Average: " + Schools.highestSchool + " " + Schools.satHighest);
+        System.out.println("Second SAT Average: " + Schools.secondSchool + " " + Schools.satSecond);
+        System.out.println("Third SAT Average: " + Schools.thirdSchool + " " + Schools.satThird);
+        System.out.println("New York State Average: " + Schools.stateAverage);
     }
+
 }
