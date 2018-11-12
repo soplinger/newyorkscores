4e2f41ab50ccfc9ce309fe182f112bdb1778645a
soplinger <so367@pvsd.org>
2018-11-12 11:21:18 -0500

    yesnlo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         String a[] = new String[100000000];
         Schools b[] = new Schools[10000000];
         int math = 0, reading = 0, writing = 0;
-        String schoolName = "";
+        String schoolName;
 
         while (s.hasNext()) {
             maxIndx++;
@@ -17,6 +17,7 @@ public class Main {
         }
 
         s.close();
+        //methods.ReadFile();
 
 
 
@@ -27,55 +28,63 @@ public class Main {
             s2.next();
             schoolName = s2.next();
 
-
-            for (int j = 0; j < 16; j++) {
+            for (int j = 0; j < 16; j++)
                 if (s2.hasNext())
                     s2.next();
-            }
 
+            String math2, reading2, writing2;
 
-            String math2 = "0", reading2 = "0", writing2 = "0";
-
-            if (s2.hasNext()) {
+            if (s2.hasNext())
+            {
                 math2 = s2.next();
-                if (math2.equals("")){
+
+                if (math2.equals(""))
                     math = 0;
-                }
-                else{
-                math = Integer.valueOf(math2);}
+                else
+                    math = Integer.valueOf(math2);
 
             }
-            if (s2.hasNext()) {
+            if (s2.hasNext())
+            {
                 reading2 = s2.next();
-                if (reading2.equals("")){
+
+                if (reading2.equals(""))
                     reading = 0;
-                }
-                else{
-                    reading = Integer.valueOf(reading2);}
+                else
+                    reading = Integer.valueOf(reading2);
             }
             if (s2.hasNext()) {
                 writing2 = s2.next();
 
-
-                if (writing2.equals("")){
+                if (writing2.equals(""))
                     writing = 0;
-                }
-                else{
-                    writing = Integer.valueOf(writing2);}
+                else
+                    writing = Integer.valueOf(writing2);
             }
+
             b[i] = new Schools(schoolName, math, reading, writing);
 
+        }
 
-        }
         System.out.println("School SAT Average");
-        for (int k = 1; k < maxIndx; k++) {
+        for (int k = 1; k < maxIndx; k++)
             System.out.println(b[k].schoolName + "\t" + b[k].satAverage);
-        }
-        System.out.println("");
-        System.out.println("Highest SAT Average: " + Schools.highestSchool + " " + Schools.satHighest);
-        System.out.println("Second SAT Average: " + Schools.secondSchool + " " + Schools.satSecond);
-        System.out.println("Third SAT Average: " + Schools.thirdSchool + " " + Schools.satThird);
-        System.out.println("New York State Average: " + Schools.stateAverage);
+
+        System.out.println("\nHighest Averages ");
+
+        System.out.println("#1 " + Schools.highestSchool + " " + Schools.satHighest);
+        if (Schools.highestSchool2 != null)
+            System.out.println("\t" + " " + Schools.highestSchool2 + " " + Schools.satHighest );
+
+        System.out.println("#2 " + Schools.secondSchool + " " + Schools.satSecond);
+        if (Schools.secondSchool2 != null)
+            System.out.println("\t" + " " + Schools.secondSchool2 + " " + Schools.satSecond );
+
+        System.out.println("#3 " + Schools.thirdSchool + " " + Schools.satThird);
+            if (Schools.thirdSchool2 != null)
+                System.out.println("\t" + " " + Schools.thirdSchool2 + " " + Schools.satThird );
+
+        System.out.println("\nNew York State Average: " + Schools.stateAverage);
     }
 
 }
--- src/com/company/Schools.java
+++ src/com/company/Schools.java
@@ -11,17 +11,21 @@ public class Schools {
     public static int satHighest;
     public static int satSecond;
     public static int satThird;
-    public static int satTotal;
+    public static double satTotal;
     public static int satCount;
     public static double stateAverage;
     public static String highestSchool;
     public static String secondSchool;
     public static String thirdSchool;
+    public static String highestSchool2 = null;
+    public static String secondSchool2 = null;
+    public static String thirdSchool2 = null;
 
     public String schoolName;
 
 
-    public Schools(String n, int a, int b, int c) {
+    public Schools(String n, int a, int b, int c)
+    {
         math = a;
         reading = b;
         writing = c;
@@ -29,21 +33,37 @@ public class Schools {
         satAverage = math + reading + writing;
         if (satAverage > satHighest){
             satThird = satSecond;
+            thirdSchool2 = secondSchool2;
+            secondSchool2 = null;
             thirdSchool = secondSchool;
             satSecond = satHighest;
+            secondSchool2 = highestSchool2;
+            highestSchool2 = null;
             secondSchool = highestSchool;
             satHighest = satAverage;
             highestSchool = schoolName;
         }
+        else if (satAverage == satHighest){
+            highestSchool2 = schoolName;
+
+        }
         else if (satAverage > satSecond){
             satThird = satSecond;
             thirdSchool = secondSchool;
+            thirdSchool2 = secondSchool2;
+            secondSchool2 = null;
             satSecond = satAverage;
             secondSchool = schoolName;
+        }
+        else if(satAverage == satSecond){
+            secondSchool2 = schoolName;
         }
         else if (satAverage > satThird){
             satThird = satAverage;
             thirdSchool = schoolName;
+        }
+        else if (satAverage == satThird){
+            thirdSchool2 = schoolName;
         }
         if (satAverage > 0){
             satCount++;
